9f8ad778e9609ddd1787ed749c5dc5b38acb80a9
Diego Armond <65828770+diegolsarmond@users.noreply.github.com>
2025-11-19 16:22:50 -0300

    Add agenda page and navigation entry

---
+++ src/pages/Agenda.tsx
@@ -0,0 +1,14 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+export default function Agenda() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Agenda</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <p>Em breve você poderá gerenciar seus agendamentos aqui.</p>
+      </CardContent>
+    </Card>
+  );
+}
